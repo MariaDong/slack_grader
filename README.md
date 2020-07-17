f52e6cb1c4fc2ebc9796207699a62f606f6f3093
Maria Dong <maria@mariadong.com>
2020-07-17 17:49:41 -0400

    Tested with Sample Data

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # slack_grader
+
+This is a simple Python tool intended for educators who would like to move
+to having asynchronous communication with students via a slack channel and 
+want to be able to print a report showing basic participation levels, including
+names, total commments, and comment contents. The report is saved as text file.
+
+To use this tool, you must be an admin of the slack channel you want to use it
+with, and you must have Python 3 installed on your computer (don't worry! That
+part is super easy!)
+
+This tool requires Python 3 to be installed on your computer.
+It should function equally well on all OS that run Python 3.
+
+SETUP:
+If you're using the free version of Slack, you can ONLY export from public 
+channels.
+
+To export from slack, you can go to https://monsters-chat.slack.com/services/export.
+If for some reason that link breaks, here's the long way around:
+
+1. Click on the workspace name at the top left corner of the screen, then 
+select "Administration" from the menu, followed by "Workspace settings."
+
+2. Hit the "Import/Export Data" tab and then select "Export."
+
+3. Make sure to select the amount of time you want to measure, e.g., 
+30 days, exact date, etc.
+
+4. The data will be saved as a .zip file. Unzip the file and move the .json file 
+from the channel you want to measure into the 'files_to_grade' folder. Move the
+users.json file into the 'user_file' folder. I recommend renaming these something
+easy to recall and type in, e.g., 'Month1.json' or 'User1.json'.
+
+5. Note: The free version of slack only saves the last 10,000 messages, so if 
+the channel is used often, you may want to run the report sooner!
+
+RUN: 
+
+Run the 'slack_grader.py' file, either from command line, double-clicking, etc.
+(Again, this won't work if you don't have Python 3 installed.) Follow the
+prompts. The program will create a file with the information you indicate in the 
+'reports' folder. Each report is named with a unique filename based on the time
+the report is run (year-month-date-hour-minute-second).txt, which makes it 
+unlikely running a new report would accidentally overwrite the previous report.
+
--- slack_grader.py
+++ slack_grader.py
@@ -1,59 +1,120 @@
 import json
 from datetime import datetime
-import time
 
-while True:
-    # Prompt for filename and append to relative file_path, save as file_name.
-    file_name_msg = 'Enter filename for chat log, e.g. monsterchat.json.'
-    file_name_msg += ' Leave blank and hit [ENTER] 2x to quit.\t'
-    file_name = input(file_name_msg)
-    if len(file_name_msg) < 1: 
-        break
-    file_path = 'files_to_grade//' + file_name
+# Prompt for filename and append to relative file_path, save as file_name.
+file_name_msg = 'Enter filename for chat log. To run sample, type "sample.json".'
+file_name_msg += ' Leave blank and hit [ENTER] 2x to quit.\t'
+file_name = input(file_name_msg)
+file_path = 'files_to_grade//' + file_name
 
-    # Prompt for users filename and append to relative user_path.
-    user_path_msg = 'Enter filename for user log, e.g. users.json.'
-    user_path_msg += 'Leave blank and hit [ENTER] to quit.\t'
-    user_file_name = input(user_path_msg)
-    if len(user_file_name) < 1: 
-        break
-    user_path = 'user_file//' + user_file_name
+# Prompt for users filename and append to relative user_path.
+user_path_msg = 'Enter filename for user log. To run sample, type "sample.json".'
+user_path_msg += 'Leave blank and hit [ENTER] to quit.\t'
+user_file_name = input(user_path_msg)
+user_path = 'user_file//' + user_file_name
 
-    # Open the user_path, parse real_names and make a pretty list.
-    user_list = []
-    with open(user_path) as users_file:
-        full_user_data = json.load(users_file)
-    print('Here is a list of all users.')
-    for user in full_user_data:
-        real_name = f"Real Name: {user['real_name']}, "
-        id_number = f"User ID: {user['id']}"
-        full = real_name + id_number
-        user_list.append(full)
-        user_list.sort()
-    print(user_list)
+# Open the user_path, parse real names and ids.
+# Make user_list, which appends a real name, user id line for text printing.
+# make user_list_real_names, which stores only the real names for dict matching.
+user_list = []
+user_list_real_names = []
+with open(user_path, encoding='utf-8') as users_file:
+    full_user_data = json.load(users_file)
+print("...Creating a user list.")
+for user in full_user_data:
+    real_name = f"Real Name: {user['real_name']}, "
+    id_number = f"Slack User ID: {user['id']}"
+    full = real_name + id_number
+    user_list.append(full)
+    user_list.sort()
+    user_list_real_names.append(user['real_name'])
+    user_list_real_names.sort()
+# print(user_list)
+# print(user_list_real_names)
 
-    # Save the user_list to a text file in reports folder.
-    # Use current date_time to second to set a unique filename.
-    now = datetime.now()
-    now = now.strftime("%Y-%m-%d-%H-%M-%S")
-    report_file = 'reports//' + now
-    time.sleep(1)
-    print('...Creating a new file for report capture.')
-    time.sleep(1)
-    with open(report_file, 'w') as working_file:
-        working_file.write(f"REPORT CREATED:\n\t{now}")
-        working_file.write(f"\nTOTAL USERS:\n\t{len(user_list)}")
-        working_file.write(f"\nLIST OF ALL USERS:\n")
-        for user in user_list:
-            working_file.write(f"\t{user}\n")
-    time.sleep(1)
-    print('...User list added to report.')
+# Save the user_list to a text file in reports folder.
+# Use current date_time to second to set a unique filename.
+now = datetime.now()
+now = now.strftime("%Y-%m-%d-%H-%M-%S")
+report_file = 'reports//' + now + '.txt'
+print('...Creating a new file for report capture.')
+with open(report_file, 'w', encoding='utf-8') as working_file:
+    working_file.write(f"REPORT CREATED:\n\t{now}")
+    working_file.write(f"\n\nTOTAL USERS:\n\t{len(user_list)}")
+    working_file.write(f"\n\nLIST OF ALL USERS:\n")
+    for user in user_list:
+        working_file.write(f"\t{user}\n")
+print('...User list added to report.')
 
-    # json_file = urllib.request.urlopen(address, context=ctx)
-    # json_data = json_file.read()
-    # json_list = json.loads(json_data)
-    # json_list = json_list['comments']
-    # comment_sum = 0
-    # for user in json_list:
-    #     comment_sum += user['count']
-    # print(comment_sum)
+# Create a dictionary to hold users. Pulling from the previously created
+# list just keeps everything organized the same way.
+comment_data = {}
+for user in user_list_real_names:
+    comment_data[user] = {
+        'Real Name': user,
+        'total comments': 0,
+        'comments':[],
+    }
+# print(comment_data)
+print("...Comment Data dictionary created.")
+
+# Open json file and prepare student_comments, a list that ignores general
+# messages like entering the chat, etc. and only contains comments actually 
+# made by users.
+student_comments = []
+with open(file_path, encoding='utf-8', errors='ignore') as comment_file:
+    full_comment_data = json.load(comment_file)
+for comment in full_comment_data:
+    if 'client_msg_id' in comment.keys():
+        student_comments.append(comment)
+
+# Use a for loop to run through each comment in student_comments and append the
+# comment text to the appropriate entry in the comment_data dictionary. Increase
+# the 'total comments' counter by 1 each time a comment is added.
+for comment in student_comments:
+    student_name = comment['user_profile']['real_name']
+    # print(student_name)
+    comment_text = comment['text']
+    # print(comment_text)
+    comment_data[student_name]['comments'].append(comment_text)
+    comment_data[student_name]['total comments'] += 1
+# print(comment_data)
+print("...Comments tabulated and added to dictionary.")
+
+# Reprint Student Name and Comment Total, but with a numbered list of comments
+# after it.
+with open(report_file, 'a', encoding='utf-8', errors='ignore') as working_file:
+    working_file.write('\nUSER COMMENT COUNTS:')
+    for user in comment_data.keys():
+        working_file.write(
+            f"\n\t{user}\t{comment_data[user]['total comments']} comments")
+    working_file.write('\n\nFULL USER COMMENT BREAKDOWN:')
+    for user in comment_data.keys():
+        comment_text_counter = 1
+        working_file.write(
+            f"\n\n{user.upper()}:\t{comment_data[user]['total comments']} COMMENTS:")
+        for comment in comment_data[user]['comments']:
+            formatted_comment = f"\n\t{comment_text_counter}. {comment}"
+            working_file.write(formatted_comment)
+            comment_text_counter += 1
+print("...Comment Data added to report.")
+print('All reports have been created. Please check the reports folder!')
+
+# # Print each username from dictionary and then print a numbered list of all comments.
+# with open(report_file, 'a', encoding='utf-8', errors='ignore') as working_file:
+#     working_file.write('\nUSER COMMENT COUNTS:')
+#     for user in user_list_real_names:
+#         working_file.write(
+#             f"\n\t{user}\t{comment_data[user]['total comments']} comments")
+#     working_file.write('\n\nFULL USER COMMENT BREAKDOWN:')
+#     for user in user_list_real_names:
+#         running_list = comment_data[user]['comments']
+#         comment_text_counter = 1
+#         working_file.write(
+#             f"\n\n{user.upper()}:\t{comment_data[user]['total comments']} COMMENTS:")
+#         for comment in running_list:
+#             formatted_comment = f"\n\t{comment_text_counter}. {comment}"
+#             working_file.write(formatted_comment)
+#             comment_text_counter += 1
+# print("...Comment Data added to report.")
+# print('All reports have been created. Please check the reports folder!')
